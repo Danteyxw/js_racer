92893053e18a095020d0c86ebcd5b0fadc4377a1
wengkhing <kheryee@live.com>
2015-09-01 14:31:31 +0800

    add custom validation for number of players

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
   has_many :game_players
+
+  validates :number_of_players_must_be_2
+
+  def number_of_players_must_be_2
+    if self.players.size != 2
+      errors.add(:number_of_players, "must be 2")
+    end
+  end
 end
